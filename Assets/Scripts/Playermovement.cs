2b602c5277ba0a5454e6239f2ba5f5b5074ade7f
lucasmuileboom <muileboomlucas@gmail.com>
2017-09-21 09:34:02 +0200

    first commit

---
+++ Assets/Scripts/Playermovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+{
+    [SerializeField]private int maxSpeed;
+    [SerializeField]private int jumpForce;
+    private int Speed = 2;
+    private Rigidbody _Rigidbody;
+
+    void Start ()
+    {
+        _Rigidbody = GetComponent<Rigidbody>();
+    }
+	void Update ()
+    {
+        if (Speed > maxSpeed)
+        {
+            Speed = maxSpeed;
+        }
+        else if (Speed < maxSpeed)
+        {
+            //Speed += ?;
+        }
+        _Rigidbody.velocity = new Vector3(Speed, _Rigidbody.velocity.y, 0);
+    }
+    public void Jump()
+    {
+        print("jump");
+        _Rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+}
---
+++ Assets/Scripts/input.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class input : MonoBehaviour
+{
+    Playermovement Playermovement;
+
+    void Start ()
+    {
+        Playermovement = GetComponent<Playermovement>();
+    }
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Playermovement.Jump();
+        }
+    }
+}
