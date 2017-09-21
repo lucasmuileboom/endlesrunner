0a4b61dd7ae8665d8c87bfdb5b4811bf03567aa6
JeffrinDumas <jeffrin.e.d@live.nl>
2017-09-21 10:06:27 +0200

    starting making a level and ordering folders

---
+++ Assets/Scripts/testScripts/Move.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+	void Update () {
+        transform.Translate(5f * Time.deltaTime, 0f, 0f);
+    }
+}
