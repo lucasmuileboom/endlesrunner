0dfa6862a5432e805fcd867d20cf1a690d26af62
JeffrinDumas <jeffrin.e.d@live.nl>
2017-09-22 11:46:31 +0200

    different level prefabs

--- Assets/Scripts/LevelScripts/LevelGen.cs
+++ Assets/Scripts/LevelScripts/LevelGen.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelGen : MonoBehaviour {
-    public Transform prefab;
-    public int numberOfObjects;
+    public static float distanceTraveled;
+    
     public Vector3 startPosition;
 
     private Vector3 nextposition;
 	// Use this for initialization
 	void Start () {
-		
-	}
+       
+        }
 	
 	// Update is called once per frame
 	void Update () {
