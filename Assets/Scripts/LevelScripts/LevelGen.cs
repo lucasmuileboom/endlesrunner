79c64c76fd60d8805be8c5db6cffeec669b14365
JeffrinDumas <jeffrin.e.d@live.nl>
2017-09-25 16:53:19 +0200

    level generator works not perfect and camera broken and fixed

--- Assets/Scripts/LevelScripts/LevelGen.cs
+++ Assets/Scripts/LevelScripts/LevelGen.cs
@@ -3,18 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelGen : MonoBehaviour {
-    public static float distanceTraveled;
-    
-    public Vector3 startPosition;
+    public List<GameObject> levelsList = new List<GameObject>();
+    public GameObject CurrentLevel;
+    private GameObject nextLevel;
+    private Vector3 levelPosition;
 
-    private Vector3 nextposition;
 	// Use this for initialization
 	void Start () {
-       
-        }
+     
+        levelPosition = new Vector3(18, 0, 0);
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(nextLevel);
+        }
 	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.CompareTag("generate"))
+        {
+            nextLevel = levelsList[Random.Range(0, 4)];
+            Debug.Log("hello people");
+            levelPosition = levelPosition + new Vector3(22, 0, 0);
+            NextLevelSpawner();
+        }
+
+        
+        
+    }
+
+    public void NextLevelSpawner()
+    {
+        GameObject levelSpawn = Instantiate(nextLevel,levelPosition,Quaternion.identity);
+    }
 }
