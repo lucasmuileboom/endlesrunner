6162a253c0359ef0e913eb58cd3dd593cca3c11f
JeffrinDumas <jeffrin.e.d@live.nl>
2017-09-26 16:43:09 +0200

    level degeneration done. ready for merge

---
+++ Assets/Scripts/LevelScripts/LevelDegen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelDegen : MonoBehaviour {
+
+    void OnCollisionEnter(Collision other)
+    {
+
+        if (other.gameObject.tag == "floor" || other.gameObject.tag == "generate")
+        {
+            Destroy(other.gameObject);
+        }
+        
+
+
+    }
+}
