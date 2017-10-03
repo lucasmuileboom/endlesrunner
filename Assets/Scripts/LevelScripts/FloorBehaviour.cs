0e5f2e3ae937c7084ea52666faf1e08cddf83b68
JeffrinDumas <jeffrin.e.d@live.nl>
2017-10-03 16:06:53 +0200

    fixing collision

---
+++ Assets/Scripts/LevelScripts/FloorBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorBehaviour : MonoBehaviour {
+    public GameObject target;
+	
+	void Update () {
+        transform.position = new Vector3(target.transform.position.x,transform.position.y, transform.position.z);
+	}
+}
