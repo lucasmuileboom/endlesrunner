a57307c1cde56c87fabdb399a6bf2642fa9e70f1
JeffrinDumas <jeffrin.e.d@live.nl>
2017-10-06 14:30:30 +0200

    adding scenes

---
+++ Assets/Scripts/UIScripts/Menus.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menus : MonoBehaviour {
+
+	// Use this for initialization
+	public void NewGameLoad () {
+        SceneManager.LoadScene(1);
+	}
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void LoadScenes()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
